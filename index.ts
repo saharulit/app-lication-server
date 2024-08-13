3540115e528666e5e350f979446679b5300f7aa3
Sahar Bracha <saharulit@gmail.com>
2024-08-13 14:52:21 +0300

    install and connect mongodb

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import { connectMongoDB } from './connect';
 
-const app = express()
+const DB_CONNECTION_STRING =
+  'mongodb+srv://saharulit:34iyjFvEzjzs1PbE@backenddb.qluby.mongodb.net/?retryWrites=true&w=majority&appName=backendDB';
+const app = express();
 
 app.listen('3000', () => {
-  console.log('Server is running on port 3000')
-})
+  console.log('Server is running on port 3000');
+});
+
+connectMongoDB(DB_CONNECTION_STRING);
+
+app.get('/', (req, res) => {
+  res.send('hey from server 22');
+});
