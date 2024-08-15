91eff58b0e40ea98cd3f67015660df5efbd1652d
Sahar Bracha <saharulit@gmail.com>
2024-08-15 15:43:31 +0300

    cleanup

--- server.ts
+++ server.ts
@@ -3,20 +3,16 @@ import { connectMongoDB } from './connect';
 import dotenv from 'dotenv';
 import appliedJobRoutes from './routes/appliedJobRoutes';
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Access environment variables
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || '';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON
+app.use(express.json());
 
-// Connect to MongoDB
 connectMongoDB(DB_CONNECTION_STRING);
 
-// Use the imported routes
 app.use('/api', appliedJobRoutes);
 
 // Test route
