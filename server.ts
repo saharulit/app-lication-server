ceb4705ff9e9493bf7dbe9d38f1312f576a97a1e
Sahar Bracha <saharulit@gmail.com>
2024-09-11 12:37:54 +0300

    add vercel address

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ app.use(express.json()); // Parse JSON bodies
 app.use(cookieParser()); // Parse cookies
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://saharulit.github.io'],
+    origin: ['http://localhost:5173', 'https://app-lication.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
