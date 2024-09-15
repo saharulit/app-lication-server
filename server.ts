a251d9b00bccc9ea776a383be7986492a832a5de
Sahar Bracha <saharulit@gmail.com>
2024-09-15 15:32:03 +0300

    test

--- server.ts
+++ server.ts
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: ['http://localhost:5173', 'https://app-lication.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
