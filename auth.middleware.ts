fac10271d73ebd5c79f56661a5b249cb76e6dbef
Sahar Bracha <saharulit@gmail.com>
2024-09-15 11:15:09 +0300

    add authenticate via Bearer token

--- auth.middleware.ts
+++ auth.middleware.ts
@@ -7,18 +7,49 @@ interface AuthRequest extends Request {
   user?: IUser;
 }
 
+// export const authenticate = async (
+//   req: AuthRequest,
+//   res: Response,
+//   next: NextFunction
+// ): Promise<void> => {
+//   const token = req.cookies.token;
+//   // console.log(`req: ${s.stringify(req)}`);
+//   if (!token) {
+//     res.status(401).json({ message: 'Not authorized, no token' });
+//     return; // Ensure that you return after sending a response
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+//       id: string;
+//     };
+
+//     // Attach the user to the request object
+//     const user = await User.findById(decoded.id).select('-password').exec();
+
+//     if (user) {
+//       req.user = user;
+//       next();
+//     } else {
+//       res.status(401).json({ message: 'Not authorized, user not found' });
+//     }
+//   } catch (error) {
+//     res.status(401).json({ message: 'Not authorized, token failed' });
+//   }
+// };
+
 export const authenticate = async (
   req: AuthRequest,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  const token = req.cookies.token;
-  // console.log(`req: ${s.stringify(req)}`);
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+
   if (!token) {
     res.status(401).json({ message: 'Not authorized, no token' });
     return; // Ensure that you return after sending a response
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
       id: string;
