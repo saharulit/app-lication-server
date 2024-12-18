88b1368130135b6a0664507984feb42e0fb61217
Sahar Bracha <saharulit@gmail.com>
2024-12-18 12:27:47 +0200

    add search companies functions

--- controllers/companyController.ts
+++ controllers/companyController.ts
@@ -1,16 +1,12 @@
 import { Request, Response } from 'express';
-import { IUser } from '../models/type';
-import {
-  getCompaniesByUserId,
-} from '../services/companyService';
+import { searchCompanyLogo } from '../services/external/logoService';
 
-export const getUserCompanies = async (req: Request, res: Response) => {
+export const searchCompanies = async (req: Request, res: Response) => {
   try {
-    const authReq = req as Request & { user: IUser };
-    const userId = authReq.user?.id;
-    const companies = await getCompaniesByUserId(userId);
+    const query = (req?.query?.query as string) || '';
+    const companies = await searchCompanyLogo(query);
     res.status(200).json(companies);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch companies by userId' });
+  } catch (e) {
+    res.status(500).json({ error: 'Failed to fetch companies' });
   }
 };
